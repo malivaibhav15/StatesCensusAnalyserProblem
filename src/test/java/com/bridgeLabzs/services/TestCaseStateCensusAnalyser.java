22f31b947bbc66af26371f621c0068ade2e4a376
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-17 12:57:41 +0530

    ADDED : Test cases for counting data

--- src/test/java/com/bridgeLabzs/services/TestCaseStateCensusAnalyser.java
+++ src/test/java/com/bridgeLabzs/services/TestCaseStateCensusAnalyser.java
@@ -1,4 +1,15 @@
 package com.bridgeLabzs.services;
+import org.junit.Assert;
+import org.junit.Test;
+import java.io.IOException;
 
-public class TestCaseStateCensusAnalyser {
+public class TestCaseStateCensusAnalyser
+{
+    @Test
+    public void givenStateCensusAnalyserFile_WhenTrue_NumberOfRecordShouldMatch() throws IOException {
+        StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+        int count = censusAnalyser.loadCensusCSVData();
+        Assert.assertEquals(29, count);
+    }
+
 }
