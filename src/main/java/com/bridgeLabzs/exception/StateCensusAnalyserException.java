6cf63a1b08ec03af4d397987efb38d05194dc41b
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-17 18:17:23 +0530

    MODIFIED : Change in the name of datatype

--- src/main/java/com/bridgeLabzs/exception/StateCensusAnalyserException.java
+++ src/main/java/com/bridgeLabzs/exception/StateCensusAnalyserException.java
@@ -1,20 +1,15 @@
 package com.bridgeLabzs.exception;
 
 public class StateCensusAnalyserException extends Exception {
-
-    public StateCensusAnalyserException(String message, ExceptionType type) {
-        super(message);
-        this.type = type;
-    }
-
     public enum ExceptionType {
-        No_SUCH_FILE_EXIST, INPUT_OUTPUT_OPERATION_FALIED;
+        FILE_NOT_FOUND,
+        DELIMITER_INCORRECT
     }
+    public ExceptionType exceptionType;
 
-    public ExceptionType type;
-
-    public StateCensusAnalyserException( Throwable cause, ExceptionType type) {
-        super(cause);
-        this.type = type;
+    public StateCensusAnalyserException (String message, ExceptionType exceptionType)
+    {
+        super(message);
+        this.exceptionType = exceptionType;
     }
 }
