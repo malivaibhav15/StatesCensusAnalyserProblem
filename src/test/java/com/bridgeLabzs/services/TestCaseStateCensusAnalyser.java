b792070e0b33848704629ce9871d114f2f7e628a
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-17 18:26:55 +0530

    ADDED : Test case for file name

--- src/test/java/com/bridgeLabzs/services/TestCaseStateCensusAnalyser.java
+++ src/test/java/com/bridgeLabzs/services/TestCaseStateCensusAnalyser.java
@@ -4,26 +4,40 @@ import com.bridgeLabzs.exception.StateCensusAnalyserException;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class TestCaseStateCensusAnalyser {
-    String CSV_FILE_PATH;
+import java.io.IOException;
 
-    @Test
-    public void givenStateCensusAnalyserFile_WhenTrue_NumberOfRecordShouldMatch() throws StateCensusAnalyserException {
-        CSV_FILE_PATH = "/home/admin1/Desktop/JAVA/StateCensusAnalyser/src/test/resources/StateCensusData.csv";
-        StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-        int count = censusAnalyser.loadCensusCSVData(CSV_FILE_PATH);
-        Assert.assertEquals(29, count);
-    }
+    public class TestCaseStateCensusAnalyser {
 
-    @Test
-    public void givenStateCensusAnalyserFile_WhenImproperFileName_ReturnsException() {
-        CSV_FILE_PATH = "/home/admin1/Desktop/JAVA/StateCensusAnalyser/src/test/resources/StateCensusData.csv";
-        StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-        try {
-            censusAnalyser.loadCensusCSVData(CSV_FILE_PATH);
-        } catch (StateCensusAnalyserException e) {
-            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.INPUT_OUTPUT_OPERATION_FALIED, e.type);
+        @Test
+        public void givenStateCensusAnalyserFile_WhenTrue_NumberOfRecordShouldMatch() throws StateCensusAnalyserException {
+            final String CSV_FILE_PATH = "/home/admin1/Desktop/JAVA/StateCensusAnalyser/src/test/resources/StateCensusData.csv";
+            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser(CSV_FILE_PATH);
+            int count = censusAnalyser.loadCensusCSVData();
+            Assert.assertEquals(29, count);
         }
+
+        @Test
+        public void givenStateCensusAnalyserFile_WhenMatched_ReturnsException() {
+            final String CSV_FILE_PATH = "/home/admin1/Desktop/JAVA/StateCensusAnalyser/src/resources/StateCensusData.csv";
+            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser(CSV_FILE_PATH);
+            try {
+                censusAnalyser.loadCensusCSVData();
+            } catch (StateCensusAnalyserException e) {
+                Assert.assertEquals(StateCensusAnalyserException.ExceptionType.FILE_NOT_FOUND, e.exceptionType);
+            }
+        }
+
+        @Test
+        public void givenStateCensusAnalyserFile_WhenWrong_ReturnsException() {
+            final String CSV_FILE_PATH = "/home/admin1/Desktop/JAVA/StateCensusAnalyser/src/test/resources/StateCensusData.txt";
+            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser(CSV_FILE_PATH);
+            try {
+                censusAnalyser.loadCensusCSVData();
+            } catch (StateCensusAnalyserException e) {
+                Assert.assertEquals(StateCensusAnalyserException.ExceptionType.FILE_NOT_FOUND, e.exceptionType);
+            }
+        }
+
+
     }
-}
 
