ee68ca4d0bdd9253a95b734d22d50976f53e5b12
malivaibhav15 <malivaibhav15@gmail.com>
2020-03-21 15:38:24 +0530

    REFACOTRED  : OpenCSVBuilder implements ICSBuilder interface

--- src/main/java/com/bridgeLabzs/services/OpenCSVBuilder.java
+++ src/main/java/com/bridgeLabzs/services/OpenCSVBuilder.java
@@ -1,4 +1,22 @@
 package com.bridgeLabzs.services;
 
-public class OpenCDVBuilder {
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.Reader;
+
+import java.util.Iterator;
+
+public class OpenCSVBuilder <E> implements ICSVBuilder {
+    //Iterator for csv file
+    @Override
+    public <E>Iterator getIterator(Reader reader, Class csvClass)
+    {
+        CsvToBean<E> csvToBean = new CsvToBeanBuilder(reader)
+                .withType(csvClass)
+                .withIgnoreLeadingWhiteSpace(true)
+                .build();
+        Iterator<E> csvUserIterator = csvToBean.iterator();
+        return csvUserIterator;
+    }
 }
