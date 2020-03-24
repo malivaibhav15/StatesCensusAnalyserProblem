d1a39c58fef535db086065984ec60dc72d1d2639
malivaibhav15 <malivaibhav15@gmail.com>
2020-03-24 17:53:34 +0530

    ADDED : Method for sorting the data

--- src/main/java/com/bridgeLabzs/services/StateCensusAnalyser.java
+++ src/main/java/com/bridgeLabzs/services/StateCensusAnalyser.java
@@ -1,24 +1,28 @@
 package com.bridgeLabzs.services;
 
 import com.bridgeLabzs.exception.CSVBuilderException;
+import com.bridgeLabzs.model.CSVStateCensus;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
 
 public class StateCensusAnalyser<E> {
     public static String CSV_FILE_PATH;
     private final Class<E> csvClass;
-    int count = 0;
+    List<E> csvUserList = null;
 
     public StateCensusAnalyser(String filePath, Class<E> csvClass) {
         CSV_FILE_PATH = filePath;
         this.csvClass = csvClass;
     }
 
+    //METHOD TO LOAD RECORDS
     public int loadCensusCSVData() throws CSVBuilderException {
         try (
                 Reader reader = Files.newBufferedReader(Paths.get(CSV_FILE_PATH))
@@ -36,5 +40,24 @@ public class StateCensusAnalyser<E> {
             e.printStackTrace();
         }
         return 0;
+    }
+
+    public String getSortedCensusData() {
+        Comparator<CSVStateCensus> CSVComparator = Comparator.comparing(census -> census.State);
+        this.sort((Comparator<E>) CSVComparator);
+        String SortedCSVJson = new Gson().toJson(csvUserList);
+        return SortedCSVJson;
+    }
+    private void sort(Comparator<E> csvComparator) {
+        for (int i = 0; i < csvUserList.size() - 1; i++) {
+            for (int j = 0; j < csvUserList.size() - i - 1; j++) {
+                E census1 = csvUserList.get(j);
+                E census2 = csvUserList.get(j + 1);
+                if (csvComparator.compare(census1, census2) > 0) {
+                    csvUserList.set(j, census2);
+                    csvUserList.set(j + 1, census1);
+                }
+            }
+        }
     }
 }
