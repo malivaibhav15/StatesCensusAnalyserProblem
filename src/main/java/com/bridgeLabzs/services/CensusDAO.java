4f0df26180b9ae981f3d5be3b55178d3a0b4d9b0
malivaibhav15 <malivaibhav15@gmail.com>
2020-03-29 15:19:01 +0530

    ADDED :

--- src/main/java/com/bridgeLabzs/services/CensusDAO.java
+++ src/main/java/com/bridgeLabzs/services/CensusDAO.java
@@ -1,4 +1,20 @@
 package com.bridgeLabzs.services;
 
+import com.bridgeLabzs.model.CSVStateCensus;
+
 public class CensusDAO {
+    public String state;
+    public int population;
+    public int area;
+    public int density;
+    public String stateCode;
+
+    public CensusDAO(CSVStateCensus csvStatesCensus) {
+        this.state = CSVStateCensus.State;
+        this.population = csvStatesCensus.Population;
+        this.area = csvStatesCensus.AreaInSqKm;
+        this.density = csvStatesCensus.DensityPerSqKm;
+    }
+
 }
+
