fcf8c9d2a20482f575ab2f83130265bb30ef4ed9
malivaibhav15 <malivaibhav15@gmail.com>
2020-03-29 16:39:14 +0530

    REFACTORED : Accomodate both pojo class

--- src/main/java/com/bridgeLabzs/services/CensusDAO.java
+++ src/main/java/com/bridgeLabzs/services/CensusDAO.java
@@ -1,20 +1,29 @@
 package com.bridgeLabzs.services;
 
 import com.bridgeLabzs.model.CSVStateCensus;
+import com.bridgeLabzs.model.CSVStateCode;
 
 public class CensusDAO {
-    public String state;
-    public int population;
-    public int area;
-    public int density;
-    public String stateCode;
+    public String State;
+    public long Population;
+    public long AreaInSqKm;
+    public int DensityPerSqkm;
+    public String StateCode;
+    public int TIN;
+    public int SrNo;
 
     public CensusDAO(CSVStateCensus csvStatesCensus) {
-        this.state = CSVStateCensus.State;
-        this.population = csvStatesCensus.Population;
-        this.area = csvStatesCensus.AreaInSqKm;
-        this.density = csvStatesCensus.DensityPerSqKm;
+        this.State = csvStatesCensus.State;
+        this.Population = csvStatesCensus.Population;
+        this.AreaInSqKm = csvStatesCensus.AreaInSqKm;
+        this.DensityPerSqkm = csvStatesCensus.DensityPerSqKm;
     }
 
-}
+    public CensusDAO(CSVStateCode csvStatesPojoClass){
+        this.State = csvStatesPojoClass.StateName;
+        this.SrNo = csvStatesPojoClass.SrNo;
+        this.TIN = csvStatesPojoClass.TIN;
+        this.StateCode = csvStatesPojoClass.StateCode;
 
+    }
+}
