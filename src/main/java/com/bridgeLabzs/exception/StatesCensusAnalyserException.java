8e86aa57a165f954114344e890a1d133549be887
malivaibhav15 <malivaibhav15@gmail.com>
2020-03-29 15:08:51 +0530

    ADDED : StatesCensusAnalyserException class

--- src/main/java/com/bridgeLabzs/exception/StatesCensusAnalyserException.java
+++ src/main/java/com/bridgeLabzs/exception/StatesCensusAnalyserException.java
@@ -1,4 +1,16 @@
 package com.bridgeLabzs.exception;
 
-public class StatesCensusAnalyserException {
+public class StatesCensusAnalyserException extends Exception {
+    public ExceptionType exceptionType;
+
+    //CONSTRUCTOR
+    public StatesCensusAnalyserException(String message, ExceptionType exceptionType) {
+        super(message);
+        this.exceptionType = exceptionType;
+    }
+
+    //ENUM CLASS
+    public enum ExceptionType {
+        FILE_NOT_FOUND, INCORRECT_FILE_TYPE, NO_CENSUS_DATA, DELIMITER_AND_HEADER_INCORRECT
+    }
 }
