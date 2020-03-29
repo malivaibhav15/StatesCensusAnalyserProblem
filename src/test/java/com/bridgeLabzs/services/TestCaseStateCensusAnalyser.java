e1e5b1bd63b4d6a1f49abc552d09e91d4b016935
malivaibhav15 <malivaibhav15@gmail.com>
2020-03-29 18:17:13 +0530

    ADDED : Test case to check sorted population

--- src/test/java/com/bridgeLabzs/services/TestCaseStateCensusAnalyser.java
+++ src/test/java/com/bridgeLabzs/services/TestCaseStateCensusAnalyser.java
@@ -1,6 +1,7 @@
 package com.bridgeLabzs.services;
 
 import com.bridgeLabzs.exception.StatesCensusAnalyserException;
+import com.bridgeLabzs.model.CSVStateCensus;
 import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,7 +11,7 @@ public class TestCaseStateCensusAnalyser {
     StateCensusAnalyser stateCensusAnalyzer = new StateCensusAnalyser();
 
     @Test
-    public void givenStateCensusAnalyserFile_WhenTrue_NumberOfRecordShouldMatch() {
+    public void givenNumberOfRecords_WhenMatched_ShouldReturnTrue() {
         final String CSV_FILE_PATH = "src/test/resources/StateCensusData.csv";
         try {
             int numberOfRecords = stateCensusAnalyzer.loadRecords(CSV_FILE_PATH);
@@ -117,9 +118,9 @@ public class TestCaseStateCensusAnalyser {
         final String CSV_FILE_PATH = "src/test/resources/StateCensusData.csv";
         try {
             stateCensusAnalyzer.loadData(CSV_FILE_PATH);
-            String SortedData = stateCensusAnalyzer.SortedCensusData();
+            String SortedData = stateCensusAnalyzer.SortedStateCensusData();
             CensusDAO[] censusCSV = new Gson().fromJson(SortedData, CensusDAO[].class);
-            Assert.assertEquals("Andhra Pradesh", censusCSV[0].state);
+            Assert.assertEquals("Andhra Pradesh", censusCSV[0].State);
         } catch (StatesCensusAnalyserException e) {
             e.getStackTrace();
         }
@@ -130,12 +131,24 @@ public class TestCaseStateCensusAnalyser {
         final String CSV_FILE_PATH = "src/test/resources/StateCode.csv";
         try {
             stateCensusAnalyzer.loadData(CSV_FILE_PATH);
-            String SortedData = stateCensusAnalyzer.SortedCensusData();
+            String SortedData = stateCensusAnalyzer.SortedStateCensusData();
             CensusDAO[] censusCSV = new Gson().fromJson(SortedData, CensusDAO[].class);
-            Assert.assertEquals("AD", censusCSV[0].stateCode);
+            Assert.assertEquals("AD", censusCSV[0].StateCode);
         } catch (StatesCensusAnalyserException e) {
             e.getStackTrace();
         }
+    }
 
+    @Test
+    public void givenStateCensusDate_WhenGivenPopulation_ShouldReturnSortedResult() {
+        final String CSV_FILE_PATH = "src/test/resources/StateCensusData.csv";
+        try {
+            stateCensusAnalyzer.loadRecords(CSV_FILE_PATH);
+            String sortedCensusData = stateCensusAnalyzer.getPopulationWiseSortedCensusData();
+            CSVStateCensus[] csvStatesCensus = new Gson().fromJson(sortedCensusData, CSVStateCensus[].class);
+            Assert.assertEquals(199812341, csvStatesCensus[0].Population);
+        } catch (StatesCensusAnalyserException e) {
+            e.printStackTrace();
+        }
     }
 }
