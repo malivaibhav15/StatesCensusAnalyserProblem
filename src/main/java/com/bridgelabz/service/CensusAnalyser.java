50762634dc81c1833684e5ef90d5f5211ce5fb59
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-01 14:23:08 +0530

    REFACTORED : Move some methods to dto package

--- src/main/java/com/bridgelabz/service/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/service/CensusAnalyser.java
@@ -1,16 +1,15 @@
-package com.bridgelabz.services;
+package com.bridgelabz.service;
 
-import com.bridgelabz.dao.CensusDAO;
 import com.bridgelabz.adapter.CensusAdapterFactory;
+import com.bridgelabz.dao.CensusDAO;
 import com.bridgelabz.exception.StatesCensusAnalyserException;
 import com.google.gson.Gson;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static java.nio.file.Files.newBufferedReader;
-
 public class CensusAnalyser<E> {
+
     List<CensusDAO> censusList = null;
     Map<String, CensusDAO> censusDAOMap = null;
 
@@ -19,13 +18,14 @@ public class CensusAnalyser<E> {
         this.censusDAOMap = new HashMap<>();
         this.censusList = new ArrayList<>();
     }
+
     public int loadStateCensusCSVData(Country country, String... csvFilePath) throws StatesCensusAnalyserException {
-        censusDAOMap =  CensusAdapterFactory.getCensusData(country, csvFilePath);
+        censusDAOMap = CensusAdapterFactory.getCensusData(country, csvFilePath);
         censusList = censusDAOMap.values().stream().collect(Collectors.toList());
         return censusDAOMap.size();
     }
 
-        //METHOD TO SORT STATE CENSUS DATA
+    //METHOD TO SORT STATE CENSUS DATA BY STATE
     public String SortedStateCensusData() throws StatesCensusAnalyserException {
         if (censusList == null || censusList.size() == 0) {
             throw new StatesCensusAnalyserException("No census data", StatesCensusAnalyserException.ExceptionType.NO_CENSUS_DATA);
@@ -48,7 +48,7 @@ public class CensusAnalyser<E> {
         return sortedStateCensusJson;
     }
 
-    //METHOD TO SORT DATA DENSITYWISE
+    //METHOD TO SORT STATE CENSUS DATA BY DENSITY
     public String getDensityWiseSortedCensusData() throws StatesCensusAnalyserException {
         if (censusList == null || censusList.size() == 0) {
             throw new StatesCensusAnalyserException("No census data", StatesCensusAnalyserException.ExceptionType.NO_CENSUS_DATA);
@@ -73,18 +73,19 @@ public class CensusAnalyser<E> {
 
     }
 
-    //METHOD TO SORT DATA
+    //METHOD TO SORT CSV DATA
     private void sortData(Comparator<CensusDAO> csvComparator) {
-        {
-            for (int i = 0; i < censusList.size() - 1; i++) {
-                for (int j = 0; j < censusList.size() - i - 1; j++) {
-                    CensusDAO census1 = censusList.get(j);
-                    CensusDAO census2 = censusList.get(j + 1);
+        for (int i = 0; i < censusList.size() - 1; i++) {
+            for (int j = 0; j < censusList.size() - i - 1; j++) {
+                CensusDAO census1 = censusList.get(j);
+                CensusDAO census2 = censusList.get(j + 1);
+                if (csvComparator.compare(census1, census2) > 0) {
                     censusList.set(j, census2);
                     censusList.set(j + 1, census1);
                 }
             }
         }
     }
+
     public enum Country {INDIA, US}
 }
