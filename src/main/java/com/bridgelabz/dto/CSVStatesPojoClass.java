d22cc49e1cc3a14de226b4eb3d77ebc89059b4ee
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-01 14:08:11 +0530

    MODIFIED : Change in the class name

--- src/main/java/com/bridgelabz/dto/CSVStatesPojoClass.java
+++ src/main/java/com/bridgelabz/dto/CSVStatesPojoClass.java
@@ -1,35 +1,35 @@
-package com.bridgeLabzs.dto;
+package com.bridgelabz.dto;
+
 
 import com.opencsv.bean.CsvBindByName;
 
 public class CSVStatesPojoClass {
+    //BINDING THE COLUMN NAME IN CsvBindByName CLASS
+    @CsvBindByName(column = "SrNo", required = true)
+    public int SrNo;
+
+    @CsvBindByName(column = "StateName", required = true)
+    public String StateName;
 
     @CsvBindByName(column = "StateCode", required = true)
     public String StateCode;
-    @CsvBindByName(column = "SrNo", required = true)
-    public int SrNo;
-    @CsvBindByName(column = "StateName", required = true)
-    public String StateName;
+
     @CsvBindByName(column = "TIN", required = true)
     public int TIN;
 
-
-    public CSVStatesPojoClass() {
-    }
-
-    public CSVStatesPojoClass(int srNo, String stateName, int TIN, String stateCode) {
+    public CSVStatesPojoClass(int srNo, String stateName, String stateCode, int TIN) {
         SrNo = srNo;
         StateName = stateName;
-        this.TIN = TIN;
         StateCode = stateCode;
+        this.TIN = TIN;
     }
 
     @Override
     public String toString() {
-        return "CSVStateCode{" +
-                "StateCode=" + StateCode +
-                ", SrNo=" + SrNo +
+        return "CSVStatesPojoClass{" +
+                "SrNo=" + SrNo +
                 ", StateName='" + StateName + '\'' +
+                ", StateCode='" + StateCode + '\'' +
                 ", TIN=" + TIN +
                 '}';
     }
